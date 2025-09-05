3b6f179ad1ae5d481d211467d6694ad7fbef00c1
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-09-05 14:58:20 +0900

    Title: DateTime to DateTime, Time: 1094ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

--- 250905/DateTime to DateTime/datetime-to-datetime.py
+++ 250905/DateTime to DateTime/datetime-to-datetime.py
@@ -4,15 +4,23 @@ a, b, c = map(int, input().split())
 def cal_elapsed_mins(a, b, c):
     cur_day, cur_hour, cur_min = 11, 11, 11
     # initial break cond
-    if a <= cur_day and b <= cur_hour and c <= cur_min:
+    if a < cur_day:
         return -1
+    elif a == cur_day:
+        if b < cur_hour:
+            return -1
+        elif b == cur_hour:
+            if c < cur_min:
+                return -1
+
     elapsed_mins = 0
+
     while True:
-        if a == cur_day and b == cur_hour and c == cur_min:
-            return elapsed_mins
-        
         elapsed_mins += 1
         cur_min += 1
+        if cur_day == a and cur_hour == b and cur_min == c:
+            return elapsed_mins
+        
         if cur_min > 59:
             cur_min = 0
             cur_hour += 1
