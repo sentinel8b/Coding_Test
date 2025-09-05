d0e1722489f28a39abc8152c3ce7efdd7515a756
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-09-05 19:35:06 +0900

    Title: 요일 맞추기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250905/요일 맞추기/guess-day-of-week.py
+++ 250905/요일 맞추기/guess-day-of-week.py
@@ -4,31 +4,23 @@ m1, d1, m2, d2 = map(int, input().split())
 
 
 def guess_day(m1, d1, m2, d2):
-    day_name = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun']
+    day_name = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     max_date_per_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     def get_day_of_yr(m, d):
-        total_days = 0
-        for month in range(1, m):
-            total_days += max_date_per_month[month]
-        total_days += d
-        return total_days
+        day = 0
+        for month in range(m):
+            day += max_date_per_month[month]
+        day+= d
+        return day
 
-    day1_of_year = get_day_of_yr(m1, d1)
-    day2_of_year = get_day_of_yr(m2, d2)
-    
-    # 두 날짜 간의 차이 계산 (m2, d2가 m1, d1보다 빠르면 음수)
-    diff_days = day2_of_year - day1_of_year
-    
-    # '월요일'을 0으로 하는 요일 인덱스
-    start_day_index = 0
-    
-    # 최종 요일 인덱스 계산 (음수 처리 포함)
-    result_index = (start_day_index + diff_days) % 7
-    if result_index < 0:
-        result_index += 7
-    
-    return day_name[result_index]
+    day_1 = get_day_of_yr(m1, d1)
+    day_2 = get_day_of_yr(m2, d2)
+
+    diff = day_2 - day_1
+    idx = diff % 7
+    return day_name[idx]
 
 ans = guess_day(m1, d1, m2, d2)
 print(ans)
+
