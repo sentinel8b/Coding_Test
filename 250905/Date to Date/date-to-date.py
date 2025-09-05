6fe6cde6a523fa7118f2ca580570ab379dbdabd1
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-09-05 14:28:47 +0900

    Title: Date to Date, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250905/Date to Date/date-to-date.py
+++ 250905/Date to Date/date-to-date.py
@@ -3,7 +3,7 @@ m1, d1, m2, d2 = map(int, input().split())
 # Please write your code here.
 days_per_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-elapsed_day = 0
+elapsed_day = 1
 
 while True:
     if m1 == m2 and d1 == d2:
