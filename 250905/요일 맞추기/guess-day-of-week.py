5775d7d820d51e238f3fdaa8f05db831a8a9e362
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-09-05 19:26:19 +0900

    Title: 요일 맞추기, Time: 72ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250905/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,29 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.m1, d1, m2, d2 = map(int, input().split())
+
+
+def guess_day(m1, d1, m2, d2):
+    day_name = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun']
+    max_date_per_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    def get_day_of_yr(m1, d1):
+        day = 0
+        for month in range(m1):
+            day += max_date_per_month[month]
+        day+= d1
+        return day
+
+    day_1 = get_day_of_yr(m1, d1)
+    day_2 = get_day_of_yr(m2, d2)
+
+    if day_2 > day_1:
+        day_to_move = (day_2 - day_1)%7
+        return day_name[day_to_move]
+    else:
+        day_to_move = (day_1 - day_2)%7
+        return day_name[7-day_to_move]
+
+ans = guess_day(m1, d1, m2, d2)
+print(ans)
+
