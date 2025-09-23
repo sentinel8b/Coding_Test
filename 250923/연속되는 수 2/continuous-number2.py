94c2f4542fc3eb5ca95d999ddfd06363dfc4913b
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-09-23 14:09:32 +0900

    Title: 연속되는 수 2, Time: 63ms, Memory: 16MB, Status: Passed - Codetree

--- 250923/연속되는 수 2/continuous-number2.py
+++ 250923/연속되는 수 2/continuous-number2.py
@@ -11,12 +11,10 @@ for i in range(len(arr)):
     elif arr[i] == arr[i-1]:
         cur_cnt += 1
     elif arr[i] != arr[i-1]:
-        if cur_cnt > max_cnt:
-            max_cnt = cur_cnt
         cur_cnt = 1
+        
+    if cur_cnt > max_cnt:
+        max_cnt = cur_cnt
 
-if cur_cnt > max_cnt:
-    max_cnt = cur_cnt
-    cur_cnt = 1
 
 print(max_cnt)
