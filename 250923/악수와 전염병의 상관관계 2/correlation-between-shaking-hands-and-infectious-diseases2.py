4e59630505ea1c375c6bc7f7894f3b01931ae4f7
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-09-23 18:39:39 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250923/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,29 @@
+N, K, P, T = map(int, input().split())
+handshakes = [tuple(map(int, input().split())) for _ in range(T)] # t, x, y
+
+infected = {P : K}
+
+status = [0] * (N+1)
+
+# Sort handshake by time order
+handshakes = sorted(handshakes, key = lambda x : (x[0], x[1], x[2]))
+
+# Iterate over handsakes following time order
+for t, x, y in handshakes:
+    # If x is infected who has remains
+    if x in infected.keys():
+        if infected[x] > 0:
+            infected[x] -= 1
+            # Add y to infected
+            if y not in infected.keys():
+                infected[y] = K
+
+# Trace who is infected
+for i in range(1, N+1):
+    if i in infected.keys():
+        print(1, end = "")
+    else:
+        print(0, end = "")
+
+
+
