0fa4d296522a96b01de7d10af9ddfad9e1506cd2
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-10-14 10:09:12 +0900

    Title: 원 모양으로 되어있는 방, Time: 73ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251014/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,18 @@
+n = int(input())
+a = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+min_value = 10000000
+# iterating over starting point
+for i in range(n):
+    dist = 0
+    assign_list = a.copy()
+    new_assign_list = assign_list[i+1:] + assign_list[:i]
+    for j in range(len(new_assign_list)):
+        dist += (j+1)*new_assign_list[j]
+
+    min_value = min(min_value, dist)
+
+print(min_value)
+
+    
