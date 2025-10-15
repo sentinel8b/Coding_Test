76627333fdc67c6a83cde3cf0b2920dee859fab8
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-10-15 19:18:45 +0900

    Title: Carry 피하기 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251015/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,27 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+max_val = 0
+
+def _check_carry_pair(num1, num2):
+    while num1 > 0 and num2 > 0:
+        digit_num1 = num1%10
+        digit_num2 = num2%10
+        if digit_num1 + digit_num2 >= 10:
+            return True
+        num1 = num1//10
+        num2 = num2//10
+    return False
+
+# Please write your code here.
+def check_carry(num1, num2, num3):
+    return True if _check_carry_pair(num1, num2) or _check_carry_pair(num1, num3) or _check_carry_pair(num2, num3) else False
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            num1, num2, num3 = arr[i], arr[j], arr[k]
+            if not check_carry(num1, num2, num3):
+                max_val = max(num1+num2+num3, max_val)
+
+print(max_val)
