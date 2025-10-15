8b0d70e5b9f53e70599e2ad6e457ce6c92a66eb2
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-10-15 19:22:49 +0900

    Title: Carry 피하기 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251015/Carry 피하기 2/escaping-carry-2.py
+++ 251015/Carry 피하기 2/escaping-carry-2.py
@@ -3,25 +3,26 @@ arr = [int(input()) for _ in range(n)]
 
 max_val = 0
 
-def _check_carry_pair(num1, num2):
-    while num1 > 0 and num2 > 0:
-        digit_num1 = num1%10
-        digit_num2 = num2%10
-        if digit_num1 + digit_num2 >= 10:
-            return True
-        num1 = num1//10
-        num2 = num2//10
+def check_carry_three(num1, num2, num3):
+    while num1 > 0 or num2 > 0 or num3 > 0:
+        digit1 = num1 % 10
+        digit2 = num2 % 10
+        digit3 = num3 % 10
+        
+        if digit1 + digit2 + digit3 >= 10:
+            return True  
+            
+        num1 //= 10
+        num2 //= 10
+        num3 //= 10
+        
     return False
-
-# Please write your code here.
-def check_carry(num1, num2, num3):
-    return _check_carry_pair(num1, num2) or _check_carry_pair(num1, num3) or _check_carry_pair(num2, num3)
 
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
             num1, num2, num3 = arr[i], arr[j], arr[k]
-            if not check_carry(num1, num2, num3):
+            if not check_carry_three(num1, num2, num3):
                 max_val = max(num1+num2+num3, max_val)
 
 if max_val == 0:
