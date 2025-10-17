12cee85821fdd7cddccb187c4127df828629fb5f
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-10-17 13:16:04 +0900

    Title: G or H 3, Time: 99ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 251017/G or H 3/G-or-H-3.py
@@ -0,0 +1,21 @@
+n, k = map(int, input().split())
+x = []
+c = []
+max_pos = 10000
+arr = [0] * (max_pos+1)
+for _ in range(n):
+    pos, char = input().split()
+    x.append(int(pos))
+    c.append(char)
+    if char == 'G':
+        arr[int(pos)] = 1
+    else:
+        arr[int(pos)] = 2
+
+max_val = 0
+k_adjusted = k+1
+# Please write your code here.
+for i in range(1,len(arr)-k_adjusted + 2):
+    max_val = max(max_val, sum(arr[i:i+k_adjusted]))
+
+print(max_val)
