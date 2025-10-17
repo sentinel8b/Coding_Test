a82ff132f1517ac6f2b480bfd69442bbf64350e3
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-10-17 16:49:14 +0900

    Title: 아름다운 수열 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251017/아름다운 수열 2/beautiful-sequence-2.py
+++ 251017/아름다운 수열 2/beautiful-sequence-2.py
@@ -7,14 +7,10 @@ B = list(map(int, input().split()))
 # Please write your code here.
 cnt = 0
 
-def get_possible_sequence(seq_list):
-    return set(permutations(seq_list, M))
+sorted_B = sorted(B)
 
-possible_seq = get_possible_sequence(B)
-
-for seq in possible_seq:
-    for i in range(len(A) - M + 1):
-        if A[i:i+M] == list(seq).copy():
-            cnt += 1
+for i in range(len(A) - M + 1):
+    if sorted(A[i:i+M]) == sorted_B:
+        cnt += 1
 
 print(cnt)
