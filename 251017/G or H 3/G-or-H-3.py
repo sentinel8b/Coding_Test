935826b6efe45a5c6dd160f28c22473e471c0b34
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-10-17 13:17:17 +0900

    Title: G or H 3, Time: 98ms, Memory: 18MB, Status: Passed - Codetree

--- 251017/G or H 3/G-or-H-3.py
+++ 251017/G or H 3/G-or-H-3.py
@@ -15,7 +15,7 @@ for _ in range(n):
 max_val = 0
 k_adjusted = k+1
 # Please write your code here.
-for i in range(1,len(arr)-k_adjusted + 2):
+for i in range(len(arr)-k_adjusted + 1):
     max_val = max(max_val, sum(arr[i:i+k_adjusted]))
 
 print(max_val)
