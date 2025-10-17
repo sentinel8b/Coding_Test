b1145322e196d1095f62d9220b094e1abc22768a
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-10-17 17:08:36 +0900

    Title: 바구니 안의 사탕 2, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 251017/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 251017/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -13,8 +13,16 @@ for _ in range(N):
 max_val = 0
 
 # Please write your code here.
-for i in range(K, len(arr)-K+1):
-    local_sum = sum(arr[i-K:i+K+1])
+for i in range(len(arr)):
+    start_idx = i-K
+    end_idx = i+K+1
+
+    if start_idx < 0:
+        start_idx = 0
+    if end_idx > len(arr):
+        end_idx = len(arr)
+
+    local_sum = sum(arr[start_idx:end_idx])
     max_val = max(max_val, local_sum)
 
 print(max_val)
