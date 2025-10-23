1a5f9e460d24de79d5862cd9cc6163ffe2fd7348
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-10-23 14:28:50 +0900

    Title: 행복한 수열의 개수, Time: 72ms, Memory: 17MB, Status: Passed - Codetree

--- 251023/행복한 수열의 개수/number-of-happy-sequence.py
+++ 251023/행복한 수열의 개수/number-of-happy-sequence.py
@@ -8,19 +8,37 @@ if m == 1:
 
 cnt = 0
 # row based
-for i in range(n):
-    for j in range(n-m):
-        for k in range(m):
+for i in range(n): # 0
+    for j in range(n-m): # 0, 1
+        local_cnt = 1
+        is_found = False
+        for k in range(m): # 0, 1, 2
             if grid[i][j+k+1] == grid[i][j+k]:
-                cnt += 1
-                break
+                local_cnt += 1
+                if local_cnt >= m:
+                    cnt += 1
+                    is_found = True
+                    break
+            else:
+                local_cnt = 1
+        if is_found:
+            break
 
 # col based
 for i in range(n):
     for j in range(n-m):
+        local_cnt = 1
+        is_found = False
         for k in range(m):
             if grid[j+k+1][i] == grid[j+k][i]:
-                cnt += 1
-                break
-
+                local_cnt += 1
+                if local_cnt >= m:
+                    cnt += 1
+                    is_found = True
+                    break
+            else:
+                local_cnt = 1
+        if is_found:
+            break
+            
 print(cnt)
