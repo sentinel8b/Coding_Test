e4b5d44547611032c5ad905b4e919c40c310fd9e
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-10-24 15:02:29 +0900

    Title: 그래프 탐색, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251024/그래프 탐색/graph-traversal.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+
+adj_mat = [[0]*(n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    start, end = map(int, input().split())
+    adj_mat[start][end] = 1
+    adj_mat[end][start] = 1
+
+node_cnt = 0
+visitied = [False] * (n+1)
+
+def dfs(start_idx):
+    global node_cnt 
+    global visitied
+    for node in range(1, n+1):
+        if adj_mat[start_idx][node] and not visitied[node]:
+            visitied[node] = True
+            dfs(node)
+
+dfs(1)
+print(sum(visitied)-1)
