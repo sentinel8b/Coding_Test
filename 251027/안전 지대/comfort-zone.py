402d2bc150e7ae1c949e0ea6d6725319782d4694
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-10-27 09:46:09 +0900

    Title: 안전 지대, Time: 894ms, Memory: 123MB, Status: Passed - Codetree

--- 251027/안전 지대/comfort-zone.py
+++ 251027/안전 지대/comfort-zone.py
@@ -1,6 +1,8 @@
+import sys
+sys.setrecursionlimit(10**7)
+
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
-
 
 # Please write your code here.
 def in_range(x, y):
