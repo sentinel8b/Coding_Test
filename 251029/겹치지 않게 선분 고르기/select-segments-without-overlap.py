f9729e082439b7a8b210c5f8122a3305d1e15d00
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-10-29 13:56:30 +0900

    Title: 겹치지 않게 선분 고르기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251029/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 251029/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -2,35 +2,30 @@ n = int(input())
 lines = [list(map(int, input().split())) for _ in range(n)]
 
 max_cnt = 0
-picked_lines = [] # lines will be added
+picked_lines = []
 
-def is_valid(line_list):
-    if len(line_list) > 1:
-        pivot = line_list[-1] # 가장 최근에 뽑은 선
-        pivot_start = pivot[0]
-        pivot_end = pivot[1]
-        for line in line_list[:-1]: # 자기 빼고 이전 선 순회하면서
-            line_start = line[0]
-            line_end = line[1]
-            if line_start <= pivot_start <= line_end or line_start <= pivot_end <= line_end:
-                return False
-        return True
-    else:
-        return True
-
-
-def pick_lines():
-    global picked_lines
+def find_max_lines(curr_idx):
     global max_cnt
-    if is_valid(picked_lines):
+
+    if curr_idx == n:
         max_cnt = max(max_cnt, len(picked_lines))
-        for item in lines:
-            picked_lines.append(item)
-            pick_lines()
-    else:
-        picked_lines.pop()
         return
 
-pick_lines()
+    find_max_lines(curr_idx + 1)
+    new_line = lines[curr_idx]
+    l1, r1 = new_line
 
+    is_compatible = True
+    for picked_line in picked_lines:
+        l2, r2 = picked_line
+        if r1 >= l2 and r2 >= l1:
+            is_compatible = False
+            break
+
+    if is_compatible:
+        picked_lines.append(new_line)
+        find_max_lines(curr_idx+1)
+        picked_lines.pop()
+
+find_max_lines(0)
 print(max_cnt)
