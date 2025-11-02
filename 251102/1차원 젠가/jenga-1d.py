eaab77d350278639f182c330708aadce5f5e69cd
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-11-02 10:24:28 +0900

    Title: 1차원 젠가, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 251102/1차원 젠가/jenga-1d.py
+++ 251102/1차원 젠가/jenga-1d.py
@@ -5,20 +5,21 @@ blocks = [int(input()) for _ in range(n)]
 s1, e1 = map(int, input().split())
 s2, e2 = map(int, input().split())
 
-temp = deepcopy(blocks)
+def cut_array(start_idx, end_idx):
+    global n, blocks
+    temp_arr = []
 
-s1 -= 1
-e1 -= 1
-s2 -= 1
-e2 -= 1
+    for i in range(n):
+        if i < start_idx or i > end_idx:
+            temp_arr.append(blocks[i])
+    
+    n = len(temp_arr)
+    for i in range(n):
+        blocks[i] = temp_arr[i]
 
-del temp[s1:e1+1]
+cut_array(s1-1, e1-1)
+cut_array(s2-1, e2-1)
 
-
-del temp[s2:e2+1]
-
-print(len(temp))
-
-
-for block_num in temp:
-    print(block_num)
+print(n)
+for i in range(n):
+    print(blocks[i])
