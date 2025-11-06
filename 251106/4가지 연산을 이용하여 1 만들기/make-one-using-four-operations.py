688347e5a2bd98f6af276acd2be683db68aa2d0f
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-11-06 15:57:45 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 103ms, Memory: 32MB, Status: Passed - Codetree

--- 251106/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 251106/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -2,49 +2,38 @@ from collections import deque
 
 n = int(input())
 
-# type 0 : -1
-# type 1 : +1
-# type 2 : if n%2 == 0 -> n // 2
-# type 3 : if n%3 == 0 -> n // 3
-
-def can_go(op_type, curr_num):
-    if op_type == 2:
-        if curr_num%2 != 0:
-            return False
-    if op_type == 3:
-        if curr_num%3 != 0:
-            return False
-
-    return True
-
-def excute_op(op_type, curr_num):
-    if op_type == 0:
-        return curr_num - 1
-    if op_type == 1:
-        return curr_num + 1
-    if op_type == 2:
-        return curr_num//2
-    if op_type == 3:
-        return curr_num//3
-
+MAX_SIZE = 2000001 
+visited = [False] * MAX_SIZE
 
 bfs_q = deque()
-for op_idx in range(4):
-    if can_go(op_idx, n):
-        bfs_q.append((op_idx, n, 0))
+bfs_q.append((n, 0))
+visited[n] = True  
 
 while bfs_q:
-    op_idx, curr_num, step = bfs_q.popleft()
+    curr_num, step = bfs_q.popleft()
 
     if curr_num == 1:
         print(step)
         break
 
-    new_num = excute_op(op_idx, curr_num)
-    for op_idx in range(4):
-        if can_go(op_idx, new_num):
-            bfs_q.append((op_idx, new_num, step+1))
+    next_num = curr_num - 1
+    if next_num > 0 and not visited[next_num]:
+        visited[next_num] = True
+        bfs_q.append((next_num, step + 1))
 
+    next_num = curr_num + 1
+    if next_num < MAX_SIZE and not visited[next_num]:
+        visited[next_num] = True
+        bfs_q.append((next_num, step + 1))
 
+    if curr_num % 2 == 0:
+        next_num = curr_num // 2
+        if not visited[next_num]:
+            visited[next_num] = True
+            bfs_q.append((next_num, step + 1))
 
-        
+    if curr_num % 3 == 0:
+        next_num = curr_num // 3
+        if not visited[next_num]:
+            visited[next_num] = True
+            bfs_q.append((next_num, step + 1))
