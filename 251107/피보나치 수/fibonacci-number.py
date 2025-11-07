9118f1e0426efa9e7a47332c0ce35af4b77fcca8
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-11-07 14:35:02 +0900

    Title: 피보나치 수, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251107/피보나치 수/fibonacci-number.py
+++ 251107/피보나치 수/fibonacci-number.py
@@ -1,16 +1,16 @@
 n = int(input())
 
-memory = [0] * 46
-
-memory[1] = 1
-memory[2] = 1
+memo = [-1] * 46
 
 def fibo(n):
+    if memo[n] != -1:
+        return memo[n]
     if n <= 2:
-        return memory[n]
-
+        memo[n] = 1
     else:
-        return fibo(n-1) + fibo(n-2)
+        memo[n] = fibo(n-1) + fibo(n-2)
+    
+    return memo[n]
 
 ans = fibo(n)
 
