b7a38ef762975d7789ad346d295545bef0347105
KyungSik Kim <rlarudtlrkks@gmail.com>
2025-12-31 17:22:07 +0900

    Title: 2진수로 변환하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251231/2진수로 변환하기/convert-to-binary.py
+++ 251231/2진수로 변환하기/convert-to-binary.py
@@ -5,16 +5,16 @@ dig = 0
 n_dig = n
 i = 0
 while True:
-    dig += 10**i * (n % 2)
-    n = n // 2
-    i += 1
-
     if n == 1:
         dig += 10**i * (n % 2)
         break
     elif n == 0:
         dig *= 10
         break
+    dig += 10**i * (n % 2)
+    n = n // 2
+    i += 1
+
     
 
 print(dig)
