0ddb54492a3de86f4daaecc1c1a993394a044c68
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-02 13:43:41 +0900

    Title: 진수 to 진수, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260102/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,15 @@
+a, b = map(int, input().split())
+n = input()
+
+# Please write your code here.
+t = 0
+for i in range(len(n)):
+    t += 8 ** i * int(n[::-1][i])
+
+tmp_list = []
+while True:
+    tmp_list.append(str(t % b))
+    t = t // b
+    if t == 0:
+        break
+print(int(''.join(tmp_list[::-1])))
