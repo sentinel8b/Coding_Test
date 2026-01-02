6d51e925601f430daab073f8c1708d89af2af270
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-02 16:25:23 +0900

    Title: 사각형의 총 넓이 2, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 260102/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 260102/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -8,8 +8,21 @@ for _ in range(n):
     y2.append(d)
 
 # Please write your code here.
+tmp_list = [[0 for _ in range(201)] for _ in range(201)]
 
-sorted_x = sorted(x1+x2)[1:3]
-sorted_y = sorted(y1+y2)[1:3]
+for i in range(len(x1)):
+    x1_tmp = x1[i] + 100
+    y1_tmp = y1[i] + 100
+    x2_tmp = x2[i] + 100
+    y2_tmp = y2[i] + 100
+    for j in range(x1_tmp, x2_tmp):
+        for k in range(y1_tmp, y2_tmp):
+            tmp_list[j][k] = 1
+    
+total_size = 0
+for i in range(len(tmp_list)):
+    for j in range(len(tmp_list[i])):
+        if tmp_list[i][j] == 1:
+            total_size += 1
 
-print((x2[0] - x1[0]) * (y2[0] - y1[0]) + (x2[1] - x1[1]) * (y2[1] - y1[1]) - (sorted_x[1] - sorted_x[0]) * (sorted_y[1] - sorted_y[0]))
+print(total_size)
