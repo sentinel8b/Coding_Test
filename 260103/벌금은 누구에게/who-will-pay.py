18ef94624fa86e640ed2beebcc75a72af9b16057
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-03 14:39:53 +0900

    Title: 벌금은 누구에게, Time: 78ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 260103/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,14 @@
+N, M, K = map(int, input().split())
+student = [int(input()) for _ in range(M)]
+
+# Please write your code here.
+
+stdt_list = [0 for _ in range(N+1)]
+
+for i in range(len(student)):
+    idx = student[i]
+    stdt_list[idx] += 1
+    if stdt_list[idx] >= K:
+        print(idx)
+        break
+print(-1)
