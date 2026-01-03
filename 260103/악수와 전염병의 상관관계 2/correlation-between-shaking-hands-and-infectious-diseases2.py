190060faffdc80e2a133a378d6de99eb7b786432
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-03 17:20:21 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260103/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,24 @@
+N, K, P, T = map(int, input().split())
+handshakes = [tuple(map(int, input().split())) for _ in range(T)]
+
+# Please write your code here.
+
+tmp_list = [0 for _ in range(N+1)]
+max_list = [0 for _ in range(N+1)]
+sorted_hs = sorted(handshakes)
+tmp_list[P] = 1
+
+for i in range(len(sorted_hs)):
+    f_man = sorted_hs[i][1]
+    s_man = sorted_hs[i][2]
+    if tmp_list[f_man] == 1 and max_list[f_man] < K:
+        tmp_list[s_man] = 1
+        max_list[f_man] += 1
+    elif tmp_list[s_man] == 1 and max_list[s_man] < K:
+        tmp_list[f_man] = 1
+        max_list[s_man] += 1
+
+result = []
+for i in tmp_list[1:]:
+    result.append(str(i))
+print(''.join(result))
