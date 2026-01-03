883699a71805ba1075b02b6c5abf7914554ec0db
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-03 13:17:53 +0900

    Title: 연속되는 수 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260103/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,16 @@
+N = int(input())
+arr = [int(input()) for _ in range(N)]
+
+# Please write your code here.
+result = 0
+for n in range(N):
+    if n == 0:
+        dup_num = 1
+    else:
+        if arr[n] == arr[n-1]:
+            dup_num += 1
+        else:
+            dup_num = 1
+        if result < dup_num:
+            result = dup_num
+print(result)
