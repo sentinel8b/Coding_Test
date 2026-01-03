a69f9c982dc24c91adeb434e2b446e180e69f142
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-03 18:53:09 +0900

    Title: 되돌아오기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260103/되돌아오기/come-back.py
@@ -0,0 +1,22 @@
+N = int(input())
+moves = [tuple(input().split()) for _ in range(N)]
+dir = [move[0] for move in moves]
+dist = [int(move[1]) for move in moves]
+
+# Please write your code here.
+dx, dy = [1, 0, 0, -1], [0, 1, -1, 0]
+dir_dict = {'W':0,'S':1,'N':2,'E':3}
+
+init_loc = [0,0]
+init_time = -1
+time_count = 0
+
+for n in range(N):
+    for d in range(dist[n]):
+        init_loc[0] += dx[dir_dict[dir[n]]]
+        init_loc[1] += dy[dir_dict[dir[n]]]
+        if init_loc[0] == 0 and init_loc[1] == 0:
+            init_time = time_count
+            break
+        time_count += 1
+print(init_time + 1)
