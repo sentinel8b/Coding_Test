d2dcc7b0579520128e0b554855f74904b34c5b9a
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-16 19:51:19 +0900

    Title: 오목, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260116/오목/O-mok.py
@@ -0,0 +1,37 @@
+board = [list(map(int, input().split())) for _ in range(19)]
+
+# Please write your code here.
+
+for i in range(19):
+    for j in range(19):
+        if i < 15:
+            if [board[i][j], board[i+1][j], board[i+2][j], board[i+3][j], board[i+4][j]] == [1, 1, 1, 1, 1]:
+                print(1)
+                print(i+2,j)
+            elif [board[i][j], board[i+1][j], board[i+2][j], board[i+3][j], board[i+4][j]] == [2, 2, 2, 2, 2]:
+                print(2)
+                print(i+2,j)
+        if j < 15:
+            if [board[i][j], board[i][j+1], board[i][j+2], board[i][j+3], board[i][j+4]] == [1, 1, 1, 1, 1]:
+                print(1)
+                print(i+1,j+3)
+            elif [board[i][j], board[i][j+1], board[i][j+2], board[i][j+3], board[i][j+4]] == [2, 2, 2, 2, 2]:
+                print(2)
+                print(i+1,j+3)
+        if i < 15 and j < 15:
+            if [board[i][j], board[i+1][j+1], board[i+2][j+2], board[i+3][j+3], board[i+4][j+4]] == [1, 1, 1, 1, 1]:
+                print(1)
+                print(i+3,j+3)
+            elif [board[i][j], board[i+1][j+1], board[i+2][j+2], board[i+3][j+3], board[i+4][j+4]] == [2, 2, 2, 2, 2]:
+                print(2)
+                print(i+3,j+3)
+        if j >= 4 and i < 15:
+            if [board[i][j], board[i+1][j-1], board[i+2][j-2], board[i+3][j-3], board[i+4][j-4]] == [1, 1, 1, 1, 1]:
+                print(1)
+                print(i+3,j-1)
+            elif [board[i][j], board[i+1][j-1], board[i+2][j-2], board[i+3][j-3], board[i+4][j-4]] == [2, 2, 2, 2, 2]:
+                print(2)
+                print(i+3,j-1)
+
+        
+        
