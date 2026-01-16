4c21e3c113865318e48de0e725646e1e68e8a1e5
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-16 19:05:46 +0900

    Title: 씨 오 더블유 2, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 260116/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,11 @@
+n = int(input())
+S = input()
+
+# Please write your code here.
+idx = 0
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            if S[i] == 'C' and S[j] == 'O' and S[k] == 'W':
+                idx += 1
+print(idx)
