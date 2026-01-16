9a9cce83ec9ba8abed3b9320a36570a46a022fcc
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-16 19:57:04 +0900

    Title: 오목, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 260116/오목/O-mok.py
+++ 260116/오목/O-mok.py
@@ -10,10 +10,10 @@ for i in range(19):
         if i < 15:
             if [board[i][j], board[i+1][j], board[i+2][j], board[i+3][j], board[i+4][j]] == [1, 1, 1, 1, 1]:
                 winner = 1
-                idx_x, idx_y = i+2,j
+                idx_x, idx_y = i+3,j+1
             elif [board[i][j], board[i+1][j], board[i+2][j], board[i+3][j], board[i+4][j]] == [2, 2, 2, 2, 2]:
                 winner = 2
-                idx_x, idx_y = i+2,j
+                idx_x, idx_y = i+3,j+1
         if j < 15:
             if [board[i][j], board[i][j+1], board[i][j+2], board[i][j+3], board[i][j+4]] == [1, 1, 1, 1, 1]:
                 winner = 1
