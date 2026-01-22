b09583cf1a0a888cd033fcfca04f4986ec9dc03a
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-22 15:12:57 +0900

    Title: 아름다운 수열 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260122/아름다운 수열 2/beautiful-sequence-2.py
+++ 260122/아름다운 수열 2/beautiful-sequence-2.py
@@ -5,10 +5,6 @@ B = list(map(int, input().split()))
 # Please write your code here.
 result = 0
 for i in range(N-M+1):
-    tmp_val = 0
-    for j in range(i, i+M):
-        if A[j] in B:
-            tmp_val += 1
-    if tmp_val == M:
+    if sorted(A[i:i+M]) == sorted(B):
         result += 1
 print(result)
