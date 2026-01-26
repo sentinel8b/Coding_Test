88083e486845365e8c95880e4d71fac66258c1f1
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-26 19:36:48 +0900

    Title: 바구니 안의 사탕 2, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260126/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 260126/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -11,20 +11,24 @@ for _ in range(N):
 import sys
 
 candy_pos = [0 for _ in range(101)]
+candy_pos_d  = [0 for _ in range(101)]
 
 for n in range(N):
     candy_pos[pos[n]] = candy[n]
+    candy_pos_d[pos[n]] += 1
 
 max_val = -sys.maxsize + 1
 
-for i in range(len(candy_pos)):
+for i in range(101):
     if i >= K:
         tmp_candy = candy_pos[i - K : i + K + 1]
+        d = candy_pos_d[i - K : i + K + 1]
     if i < K:
         tmp_candy = candy_pos[0 : i + K + 1]
+        d = candy_pos_d[0 : i + K + 1]
     sum = 0
-    for t in tmp_candy:
-        sum += t
+    for j in range(len(tmp_candy)):
+        sum += tmp_candy[j] * d[j]
     max_val = max(max_val, sum)
 
 print(max_val)
