8fec05dc3bf1ad37a90e50daea39d2da4a4fa718
KyungSik Kim <rlarudtlrkks@gmail.com>
2026-01-27 20:41:27 +0900

    Title: 트로미노, Time: 112ms, Memory: 19MB, Status: Passed - Codetree

--- 260127/트로미노/tromino.py
+++ 260127/트로미노/tromino.py
@@ -7,7 +7,7 @@ import sys
 max_b1 = -sys.maxsize + 1
 for i in range(n):
     for j in range(m):
-        if j + 2 < n:
+        if j + 2 < m:
             sum = grid[i][j] + grid[i][j+1] + grid[i][j+2]
             max_b1 = max(max_b1, sum)
         if i + 2 < n:
@@ -17,12 +17,12 @@ for i in range(n):
 max_b2 = -sys.maxsize + 1
 for i in range(n):
     for j in range(m):
-        if i+1 < n and j+1 < n:
+        if i+1 < n and j+1 < m:
             sum1 = grid[i][j] + grid[i+1][j] + grid[i+1][j+1]
             sum2 = grid[i][j] + grid[i][j+1] + grid[i+1][j+1]
             max_val = max(sum1, sum2)
             max_b2 = max(max_b2, max_val)
-        if i-1 >= 0 and j+1 < n:
+        if i-1 >= 0 and j+1 < m:
             sum1 = grid[i][j] + grid[i][j+1] + grid[i-1][j+1]
             sum2 = grid[i][j] + grid[i-1][j] + grid[i-1][j+1]
             max_val = max(sum1, sum2)
